1534f200dc3bff3fc06d5fef8945ede19877c3ce
Emil Eriksson <98emil.eriksson@gmail.com>
2025-04-24 23:53:50 +0200

    load film data from JSON file

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 // Set up burgerMenu from Sean
+const burgerMenu = document.getElementById("burgermenu");
+const mobileMenu = document.getElementById("mobileMenu");
 
-const burgerMenu = document.getElementById('burgermenu');
-const mobileMenu = document.getElementById('mobileMenu');
-
-burgerMenu.addEventListener('click',()=>{
-    burgerMenu.classList.toggle('active');
-    mobileMenu.classList.toggle('show');
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
+  mobileMenu.classList.toggle("show");
 });
+
+// Data loading
+function loadFilms() {
+  fetch("data/films.json")
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      console.log("Films have been loaded");
+    });
+}
+
+function loadHandler() {
+  loadFilms();
+}
+
+window.addEventListener("load", loadHandler);
