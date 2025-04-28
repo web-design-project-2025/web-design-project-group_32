598e01eaa4ca2c33573a2d30e1515478ae413012
Yanshishuang <yash24oy@student.ju.se>
2025-04-28 21:48:29 +0200

    change the name

--- js/film-detial.js
+++ js/film-detial.js
@@ -1,5 +1,5 @@
 // Get the comments section data
-fetch("../json/comments.json")
+fetch("../data/comments.json")
   .then((response) => response.json())
   .then((comments) => {
     const container = document.getElementById("comments-container");
