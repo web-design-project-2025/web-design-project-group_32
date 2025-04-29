60115896f32947f33c070ba5ac481c4ddd450a10
Binu993 <sebastian.binu993@gmail.com>
2025-04-29 11:33:41 +0530

    New image updation for hero section using json and javascript.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,26 @@ burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.toggle("active");
   mobileMenu.classList.toggle("show");
 });
+
+function loadHandler() {
+  fetch("json/hero.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const lastIndex = Number(localStorage.getItem("index"));
+
+      let randomIndex = Math.floor(Math.random() * data.length);
+
+      while (randomIndex === lastIndex) {
+        randomIndex = Math.floor(Math.random() * data.length);
+      }
+
+      const randomImage = data[randomIndex];
+
+      localStorage.setItem("index", randomIndex);
+
+      const heroSection = document.getElementById("hero-section");
+      heroSection.style.backgroundImage = "url('hero-img/" + randomImage + "')";
+    });
+}
+
+window.addEventListener("load", loadHandler);
